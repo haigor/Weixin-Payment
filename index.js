e9e0c4706d64a5d185c3737cfcc9de414fd0edf4
Peter <wxd_sc_dy@163.com>
2016-03-09 19:29:38 +0800

    完成统一下单

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 var tools = require('./lib/tools');
 
 module.exports = {
-    unifiedOrder: function(options, _timeout) {
+    unifiedOrder: function(options, key, _timeout) {
         var _url = 'https://api.mch.weixin.qq.com/pay/unifiedorder',
             info = {
                 status: true,
@@ -25,14 +25,26 @@ module.exports = {
         } else if (!tools.isNotNull(options.appid)) {
             throw ('缺少统一支付接口必填参数appid！');
         } else if (!tools.isNotNull(options.mchid)) {
-            throw('缺少统一支付接口必填参数mchid！');
+            throw ('缺少统一支付接口必填参数mchid！');
         } else if (!tools.isNotNull(options.notify_url)) {
-            throw('缺少统一支付接口必填参数notify_url！');
+            throw ('缺少统一支付接口必填参数notify_url！');
         } else if (!tools.isNotNull(options.spbill_create_ip)) {
-            throw('缺少统一支付接口必填参数spbill_create_ip！');
+            throw ('缺少统一支付接口必填参数spbill_create_ip！');
+        } else if (!tools.isNotNull(key)) {
+            throw ('缺少统一支付接口必填参数spbill_create_ip！');
         }
 
-        //检测关联参数
+        //关联参数
+        if (options.trade_type === "JSAPI" && tools.isNotNull(options.openid)) {
+            throw ("统一支付接口中，缺少必填参数openid！trade_type为JSAPI时，openid为必填参数！");
+        }
+        if (options.trade_type === "NATIVE" && tools.isNotNull(options.product_id)) {
+            throw ("统一支付接口中，缺少必填参数product_id！trade_type为JSAPI时，product_id为必填参数！");
+        }
+
+        options.nonce_str = tools.getNonceStr();
+
+        options.sign = tools.getSign(options);
 
         return info;
     }
